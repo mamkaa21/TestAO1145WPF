7a059fac952b4f007ee1672fed562465e922fdf6
Mamka21 <mamka21a@mail.ru>
2026-06-12 10:10:31 +0300

    ffff

--- TestAO1145WPF/ViewModel/AdminWinVM.cs
+++ TestAO1145WPF/ViewModel/AdminWinVM.cs
@@ -87,21 +87,22 @@ namespace TestAO1145WPF.ViewModel
 
         JsonSerializerOptions options = new JsonSerializerOptions();
         public AdminWinVM() {
-            timerStart();
+            //timerStart();
+            GetAllTeacher();
+            GetAllStudent();
+            GetAllSubject();
             //тут добавить метод получения учителей студентов и предметов с апи 
             OpenAddTeacher = new Command(async () =>
             {
                 AddTeacherWin    addTeacherWin = new AddTeacherWin();
                 addTeacherWin.Show();
-                Signal();
-                
+                Signal();   
             });
             OpenDelete = new Command(async () =>
             {
                 //MainWindow mainWindow = new MainWindow();
                 //mainWindow.Show();
                 Signal();
-
             });
         }
         public async void GetAllTeacher()
@@ -125,7 +126,7 @@ namespace TestAO1145WPF.ViewModel
         public async void GetAllStudent()
         {
             string arg = JsonSerializer.Serialize(Student);
-            var responce = await HttpClients.HttpClient.GetAsync($"Student/GetAllTest");
+            var responce = await HttpClients.HttpClient.GetAsync($"Admin/GetAllStudent");
 
             if (responce.StatusCode == System.Net.HttpStatusCode.BadRequest)
             {
@@ -139,21 +140,41 @@ namespace TestAO1145WPF.ViewModel
                 return;
             }
         }
-        public void timerStart()
+
+        public async void GetAllSubject()
         {
-            timer = new DispatcherTimer();
-            timer.Tick += new EventHandler(timerTick);
-            timer.Interval = new TimeSpan(0, 0, 10);
-            timer.Start();
+            string arg = JsonSerializer.Serialize(Subject);
+            var responce = await HttpClients.HttpClient.GetAsync($"Admin/GetAllSubject");
+
+            if (responce.StatusCode == System.Net.HttpStatusCode.BadRequest)
+            {
+                var result = await responce.Content.ReadAsStringAsync();
+                MessageBox.Show(result);
+                return;
+            }
+            if (responce.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                SubjectList = await responce.Content.ReadFromJsonAsync<List<Subject>>();
+                return;
+            }
         }
-        private void timerTick(object sender, EventArgs e) //к таймеру относится 
-        {
-            Thread thread1 = new Thread(GetAllStudent);
-            thread1.Start();
-            Thread thread2 = new Thread(GetAllTeacher);
-            thread2.Start();
-            //timer.Stop();
-        }
+        //public void timerStart()
+        //{
+        //    timer = new DispatcherTimer();
+        //    timer.Tick += new EventHandler(timerTick);
+        //    timer.Interval = new TimeSpan(0, 0, 10);
+        //    timer.Start();
+        //}
+        //private void timerTick(object sender, EventArgs e) //к таймеру относится 
+        //{
+        //    Thread thread1 = new Thread(GetAllStudent);
+        //    thread1.Start();
+        //    Thread thread3 = new Thread(GetAllSubject);
+        //    thread1.Start();
+        //    Thread thread2 = new Thread(GetAllTeacher);
+        //    thread2.Start();
+        //    //timer.Stop();
+        //}
 
         internal void SetWindow(AdminWin adminWin)
         {
