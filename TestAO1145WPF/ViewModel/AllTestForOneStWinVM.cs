599bceda9e6baa601c46271c21e4f7eee571289a
Mamka21 <mamka21a@mail.ru>
2026-06-15 03:34:03 +0300

    fff

--- TestAO1145WPF/ViewModel/AllTestForOneStWinVM.cs
+++ TestAO1145WPF/ViewModel/AllTestForOneStWinVM.cs
@@ -1,12 +1,101 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Json;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Threading;
+using TestAO1145WPF.Model;
+using TestAO1145WPF.View;
 
 namespace TestAO1145WPF.ViewModel
 {
-    internal class AllTestForOneStWinVM
+     class AllTestForOneStWinVM : BaseVM
     {
+        private Test test { get; set; }
+        public Test Test
+        {
+            get => test;
+            set
+            {
+                test = value;
+                Signal(nameof(Test));
+            }
+        }
+        private List<Test> testList { get; set; }
+        public List<Test> TestList
+        {
+            get => testList;
+            set
+            {
+                testList = value;
+                Signal(nameof(TestList));
+            }
+        }
+        private DispatcherTimer timer = null;
+        public Command Back { get; }
+        public ICommand DoubleClickCommand { get; private set; }
+        public AllTestForOneStWinVM()
+        {
+            timerStart();
+            Back = new Command(async () =>
+            {
+                TeacherWin teacherWin = new TeacherWin();
+                teacherWin.Show();
+                //CloseWindow();
+            });
+            DoubleClickCommand = new RelayCommand(DoubleClickExecute);
+        }
+        private void DoubleClickExecute(object parameter)
+        {
+
+            if (parameter is Test Test)
+            {
+                //TestWin goodWin = new TestWin(Test);
+                //goodWin.Show();
+                //Signal();
+            }
+        }
+        public async void GetAllTest() //в апи длястудента тесты только где его айдм
+        {
+            string arg = JsonSerializer.Serialize(Test);
+            var responce = await HttpClients.HttpClient.GetAsync($"Student/GetAllTest");
+
+            if (responce.StatusCode == System.Net.HttpStatusCode.BadRequest)
+            {
+                var result = await responce.Content.ReadAsStringAsync();
+                MessageBox.Show(result);
+                return;
+            }
+            if (responce.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                TestList = await responce.Content.ReadFromJsonAsync<List<Test>>();
+                return;
+            }
+        }
+        public void timerStart()
+        {
+            timer = new DispatcherTimer();
+            timer.Tick += new EventHandler(timerTick);
+            timer.Interval = new TimeSpan(0, 0, 10);
+            timer.Start();
+        }
+        private void timerTick(object sender, EventArgs e) //к таймеру относится 
+        {
+            Thread thread1 = new Thread(GetAllTest);
+            thread1.Start();
+        }
+        AllTestForOneStWin allTestForOneStWin;
+        internal void SetWindow(AllTestForOneStWin allTestForOneStWin)
+        {
+            this.allTestForOneStWin = allTestForOneStWin;
+        }
+        internal void CloseWindow() //fix no work
+        {
+            this.allTestForOneStWin.Close();
+        }
     }
 }
--- TestAO1145WPF/ViewModel/CreateTestWinVM.cs
+++ TestAO1145WPF/ViewModel/CreateTestWinVM.cs
@@ -3,10 +3,42 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TestAO1145WPF.Model;
 
 namespace TestAO1145WPF.ViewModel
 {
-    class CreateTestWinVM
+    class CreateTestWinVM : BaseVM
     {
+        private Test test { get; set; }
+        public Test Test
+        {
+            get => test;
+            set
+            {
+                test = value;
+                Signal(nameof(Test));
+            }
+        }
+        private List<Test> testList { get; set; }
+        public List<Test> TestList
+        {
+            get => testList;
+            set
+            {
+                testList = value;
+                Signal(nameof(TestList));
+            }
+        }
+        private Student student { get; set; }
+        public Student Student
+        {
+            get => student;
+            set
+            {
+                student = value;
+                Signal(nameof(student));
+            }
+        }
     }
+
 }
